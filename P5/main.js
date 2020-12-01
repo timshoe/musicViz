c1d394d53d0c76b63cb61c36d56e87c238874bfe
Liam Fernandez <liamdfernandez@gmail.com>
2020-12-01 15:40:18 -0500

    Removed the flufff

--- P5/main.js
+++ P5/main.js
@@ -1,8 +1,43 @@
-var width = 500;
-var height = 500;
+
+//*All work & code contributed solely by Liam Fernandez */
+window.onload = start;
+var sourceOfTruth = [];
+
+function start() {
+  var width = 900;
+  var height = 700;
+
+  var margin = {
+    top: 30,
+    bottom: 30,
+    left: 50,
+    right: 50
+  }
+
+
+  var graph = document.getElementById('graph');
+
+
+  var svg = d3.select(graph)
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height);
+
+    var xScale = d3.scaleLinear().range([margin.left, width - margin.right]);
+    var yScale = d3.scaleBand().rangeRound([margin.bottom, height-margin.top], 0.3);
+
+
+
+    
+}
+
+
 
 d3.csv("songattributes.csv", function (csv) {
   for (var i = 0; i < csv.length; ++i) {
+    csv[i].Title = String(csv[i].Title);
+    csv[i].Artist = String(csv[i].Artist);
+    csv[i].Year = Number(csv[i].Year); // This is default x-axis value
     csv[i].BeatsPerMinute = Number(csv[i].BeatsPerMinute);
     csv[i].Energy = Number(csv[i].Energy);
     csv[i].Danceability = Number(csv[i].Danceability);
@@ -34,14 +69,8 @@ d3.csv("songattributes.csv", function (csv) {
   var xScale = d3.scaleLinear().domain(fatExtent).range([50, 470]);
   var yScale = d3.scaleLinear().domain(carbExtent).range([470, 30]);
 
-  var xScale2 = d3.scaleLinear().domain(fiberExtent).range([50, 470]);
-  var yScale2 = d3.scaleLinear().domain(proteinExtent).range([470, 30]);
-
   var xAxis = d3.axisBottom().scale(xScale);
   var yAxis = d3.axisLeft().scale(yScale);
-
-  var xAxis2 = d3.axisBottom().scale(xScale2);
-  var yAxis2 = d3.axisLeft().scale(yScale2);
 
   function createGraph(chart1) {
     chart1
@@ -69,86 +98,7 @@ d3.csv("songattributes.csv", function (csv) {
     .select("#chart1")
     .append("svg:svg")
     .attr("width", width)
-    .attr("height", height)
-    .on("click", function (d, i) {
-      createGraph(chart1)
-    });
-  var chart2 = d3
-    .select("#chart2")
-    .append("svg:svg")
-    .attr("width", width)
     .attr("height", height);
-
-  // clears clicked values
-  $(document).ready(function () {
-    $("div").click(function (event) {
-      if (event.target.nodeName != "circle") {
-        document.getElementById('Fat').innerHTML = ""
-        document.getElementById('Carb').innerHTML = ""
-        document.getElementById('Fiber').innerHTML = ""
-        document.getElementById('Protein').innerHTML = ""
-        var chart1C = document.getElementById("chart1").getElementsByTagName("circle");
-        var chart2C = document.getElementById("chart2").getElementsByTagName("circle");
-        // circles1.classed("selected2", false);
-        for (var j = 0; j < chart1C.length; j++) {
-          // chart1C[j].style.fill = "black";
-          // chart1C[j].style.stroke = "black";
-          // chart2C[j].style.stroke = "black";
-          // chart2C[j].style.fill = "black";
-          chart1C[j].style.opacity = 1;
-          chart2C[j].style.opacity = 1;
-        }
-      }
-    });
-  });
-
-  /******************************************
-  	
-    ADD BRUSHING CODE HERE
-
-   ******************************************/
-  chart1
-    .call(d3.brush()                 // Add the brush feature using the d3.brush function
-      .extent([[0, 0], [width, height]]) // initialise the brush area: start at 0,0 and finishes at width,height: it means I select the whole graph area
-      .on("start brush", updateChart) // Each time the brush selection changes, trigger the 'updateChart' function
-    )
-
-  chart2
-  .call(d3.brush()                 // Add the brush feature using the d3.brush function
-    .extent([[0, 0], [width, height]]) // initialise the brush area: start at 0,0 and finishes at width,height: it means I select the whole graph area
-    .on("start brush", updateChart2) // Each time the brush selection changes, trigger the 'updateChart' function
-  )
-
-  // Function that is triggered when brushing is performed
-  function updateChart() {
-    extent = d3.event.selection
-    circles1.classed("selected2", function (d) { 
-      return isBrushed(extent, xScale(d.Fat), yScale(d.Carb)) 
-    })
-    circles2.classed("selected2", function (d) { 
-      return isBrushed(extent, xScale(d.Fat), yScale(d.Carb)) 
-    })
-  }
- 
-
-  function updateChart2() {
-    extent = d3.event.selection
-    circles1.classed("selected2", function (d) { 
-      return isBrushed(extent, xScale2(d.Fiber), yScale2(d.Protein)) 
-    })
-    circles2.classed("selected2", function (d) { 
-      return isBrushed(extent, xScale2(d.Fiber), yScale2(d.Protein)) 
-    })
-  }
-
-  // A function that return TRUE or FALSE according if a dot is in the selection or not
-  function isBrushed(brush_coords, cx, cy) {
-    var x0 = brush_coords[0][0],
-      x1 = brush_coords[1][0],
-      y0 = brush_coords[0][1],
-      y1 = brush_coords[1][1];
-    return x0 <= cx && cx <= x1 && y0 <= cy && cy <= y1;    // This return TRUE or FALSE depending on if the points is in the selected area
-  }
 
   //add scatterplot points
   var circles1 = chart1
@@ -181,35 +131,6 @@ d3.csv("songattributes.csv", function (csv) {
       // document.getElementById(i).style.fill = "pink";
     });
 
-  var circles2 = chart2
-    .selectAll("circle")
-    .data(csv)
-    .enter()
-    .append("circle")
-    .attr("id", function (d, i) {
-      return i;
-    })
-    .attr("class", "cir_2")
-    .attr("fill", "black")
-    .attr("stroke", "black")
-    .attr("cx", function (d) {
-      return xScale2(d.Fiber);
-    })
-    .attr("cy", function (d) {
-      return yScale2(d.Protein);
-    })
-    .attr("r", 5)
-    .on("click", function (d, i) {
-      var chart2 = document.getElementById("chart2");
-      var circles = chart2.getElementsByTagName("circle");
-      // circles[i].style.fill = "orange";
-      // circles[i].style.stroke = "orange";
-      circles[i].style.opacity = .5;
-      document.getElementById('Fat').innerHTML = d.Fat
-      document.getElementById('Carb').innerHTML = d.Carb
-      document.getElementById('Fiber').innerHTML = d.Fiber
-      document.getElementById('Protein').innerHTML = d.Protein
-    });
 
   chart1 // or something else that selects the SVG element in your visualizations
     .append("g") // create a group node
@@ -226,27 +147,6 @@ d3.csv("songattributes.csv", function (csv) {
     .append("g") // create a group node
     .attr("transform", "translate(50, 0)")
     .call(yAxis)
-    .append("text")
-    .attr("class", "label")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", ".71em")
-    .style("text-anchor", "end");
-
-  chart2 // or something else that selects the SVG element in your visualizations
-    .append("g") // create a group node
-    .attr("transform", "translate(0," + (width - 30) + ")")
-    .call(xAxis2)
-    .append("text")
-    .attr("class", "label")
-    .attr("x", width - 16)
-    .attr("y", -6)
-    .style("text-anchor", "end");
-
-  chart2 // or something else that selects the SVG element in your visualizations
-    .append("g") // create a group node
-    .attr("transform", "translate(50, 0)")
-    .call(yAxis2)
     .append("text")
     .attr("class", "label")
     .attr("transform", "rotate(-90)")
