b060bfbdb9a7350b6695c666aaaba83ae747eff7
Liam Fernandez <liamdfernandez@gmail.com>
2020-12-06 13:53:50 -0500

    cvchanged on click div

--- P5/main.js
+++ P5/main.js
@@ -426,7 +426,7 @@ function drawGraph() {
 
 }
 $(document).ready(function () {
-  $(".chart1").click(function (event) {
+  $("#graph").click(function (event) {
     if (event.target.nodeName != "circle") {
       removeOldBars();
     }
