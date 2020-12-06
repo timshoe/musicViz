fa496063f35b754ba9e68c32b626eeca4fbcd9d8
Liam Fernandez <liamdfernandez@gmail.com>
2020-12-06 13:43:27 -0500

    only clears when clicking inside scatter plot but not on a point

--- P5/main.js
+++ P5/main.js
@@ -426,7 +426,7 @@ function drawGraph() {
 
 }
 $(document).ready(function () {
-  $("svg").click(function (event) {
+  $(".chart1").click(function (event) {
     if (event.target.nodeName != "circle") {
       removeOldBars();
     }
